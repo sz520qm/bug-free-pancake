3bc7b7680d45d9c36e65f61a96a4425228a6b187
sz520qm <80267410+sz520qm@users.noreply.github.com>
2021-05-20 20:58:40 +0200

    auth routes added succesfully

--- lilyMultiAuth/app/Http/Controllers/DashboardController.php
+++ lilyMultiAuth/app/Http/Controllers/DashboardController.php
@@ -3,8 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
-    //
-}
+    public function index()
+    {
+        if(Auth::user()->hasRole('user')){
+             return view('userdash');
+        }elseif(Auth::user()->hasRole('blogwriter')){
+             return view('blogwriterdash');
+        }elseif(Auth::user()->hasRole('admin')){
+         return view('dashboard');
+    }
+    }
+ 
+    public function myprofile()
+    {
+     return view('myprofile');
+    }
+ 
+    public function postcreate()
+    {
+     return view('postcreate');
+    }
+ }
+
