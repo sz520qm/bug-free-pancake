21f5fddf4a32fc8f5485afecb8f4c807798c57bd
sz520qm <80267410+sz520qm@users.noreply.github.com>
2021-05-20 17:45:24 +0200

    add register as role form dropdown select

--- lilyMultiAuth/app/Http/Controllers/Auth/RegisteredUserController.php
+++ lilyMultiAuth/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -45,6 +45,8 @@ class RegisteredUserController extends Controller
             'password' => Hash::make($request->password),
         ]);
 
+        $user->attachRole('user');
+
         event(new Registered($user));
 
         Auth::login($user);
