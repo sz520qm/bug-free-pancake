3c0aafa76d432ec18ade00c3152c18268c5f87df
sz520qm <80267410+sz520qm@users.noreply.github.com>
2021-05-20 23:09:23 +0200

    add room to db

--- lilyMultiAuth/app/Http/Controllers/Auth/RegisteredUserController.php
+++ lilyMultiAuth/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -42,6 +42,7 @@ class RegisteredUserController extends Controller
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
+            'room_id' => $request->room_id,
             'password' => Hash::make($request->password),
         ]);
 
