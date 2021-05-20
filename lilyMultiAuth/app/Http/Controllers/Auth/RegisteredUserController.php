a33a9adcf4c18d2180af55f788ff1eb1c02f6748
sz520qm <80267410+sz520qm@users.noreply.github.com>
2021-05-20 23:09:29 +0200

    Revert "add room to db"
    This reverts commit 3c0aafa76d432ec18ade00c3152c18268c5f87df.

--- lilyMultiAuth/app/Http/Controllers/Auth/RegisteredUserController.php
+++ lilyMultiAuth/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -42,7 +42,6 @@ class RegisteredUserController extends Controller
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
-            'room_id' => $request->room_id,
             'password' => Hash::make($request->password),
         ]);
 
