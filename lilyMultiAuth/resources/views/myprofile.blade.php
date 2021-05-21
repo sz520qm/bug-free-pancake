88f9ab2fe37c2b0d8755ad5b3a49228bc8c5944f
sz520qm <80267410+sz520qm@users.noreply.github.com>
2021-05-21 17:26:32 +0200

    add rating stars

--- lilyMultiAuth/resources/views/myprofile.blade.php
+++ lilyMultiAuth/resources/views/myprofile.blade.php
@@ -14,59 +14,60 @@
                     Your email addrress: {{Auth::user()->email}}
                 </div>
                 <div class="wrapper review-details create-review">
-
-                       
-                      
-                      
+                     
                     <form action="/reviews" method="POST">
                   
                       @csrf
                       <input style="display: none" type="text" value="{{Auth::user()->name}}" name="name">
 
                     <input style="display: none"  type="text" value="{{Auth::user()->room}}" name="room">
-                 
-                      <div>
-                           {{-- <img src="{{ asset('img/bedroom.png') }}"  style="max-height: 50px">   --}}
-                          <label for="bedroom">Bedroom</label>
-                           
-                          <select name="bedroom" id="bedroom">
-                              <option value="1">1 star</option>
-                              <option value="2">2 stars</option>
-                              <option value="3">3 stars</option>
-                              <option value="4">4 stars</option>
-                              <option value="5">5 stars</option>           
-                          </select>
-                         
-                          <label for="bathroom">Bathroom</label>    
-                          <select name="bathroom" id="bathroom">
-                              <option value="1">1 star</option>
-                              <option value="2">2 stars</option>
-                              <option value="3">3 stars</option>
-                              <option value="4">4 stars</option>
-                              <option value="5">5 stars</option>           
-                          </select>
-                      
-                          <label for="livingroom">Livingroom</label>    
-                          <select name="livingroom" id="livingroom">
-                              <option value="1">1 star</option>
-                              <option value="2">2 stars</option>
-                              <option value="3">3 stars</option>
-                              <option value="4">4 stars</option>
-                              <option value="5">5 stars</option>           
-                          </select>
-                       {{-- <img src="{{ asset('img/kitchen.png') }}"  style="max-height: 50px">  --}}
-                          <label for="kitchen">Kitchen</label>    
-                          <select name="kitchen" id="kitchen">
-                              <option value="1">1 star</option>
-                              <option value="2">2 stars</option>
-                              <option value="3">3 stars</option>
-                              <option value="4">4 stars</option>
-                              <option value="5">5 stars</option>           
-                          </select>
-                        
-                          </div>
-                          
-                          <fieldset>
+
+                    <div>
+                        <fieldset class="rating1">
+                            <legend>Bedroom</legend>
+                            <input type="radio" id="1star5" name="bedroom" value="5" /><label for="1star5" title="Rocks!">5</label>
+                            <input type="radio" id="1star4" name="bedroom" value="4" /><label for="1star4" title="Pretty good">4</label>
+                            <input type="radio" id="1star3" name="bedroom" value="3" /><label for="1star3" title="Meh">3</label>
+                            <input type="radio" id="1star2" name="bedroom" value="2" /><label for="1star2" title="Kinda bad">2</label>
+                            <input type="radio" id="1star1" name="bedroom" value="1" /><label for="1star1" title="Sucks big time">1</label>
+                        </fieldset>
+                    </div>
+<div class="duties"></div>
+                    <div>
+                        <fieldset class="rating1">
+                            <legend>Bathroom</legend>
+                            <input type="radio" id="2star5" name="bathroom" value="5" /><label for="2star5" title="Rocks!"></label>
+                            <input type="radio" id="2star4" name="bathroom" value="4" /><label for="2star4" title="Pretty good"></label>
+                            <input type="radio" id="2star3" name="bathroom" value="3" /><label for="2star3" title="Meh"></label>
+                            <input type="radio" id="2star2" name="bathroom" value="2" /><label for="2star2" title="Kinda bad"></label>
+                            <input type="radio" id="2star1" name="bathroom" value="1" /><label for="2star1" title="Sucks big time"></label>
+                        </fieldset>
+                    </div>
+                    <div class="duties"></div>
+                    <div>
+                        <fieldset class="rating1">
+                            <legend>Living Room</legend>
+                            <input type="radio" id="3star5" name="livingroom" value="5" /><label for="3star5" title="Rocks!"></label>
+                            <input type="radio" id="3star4" name="livingroom" value="4" /><label for="3star4" title="Pretty good"></label>
+                            <input type="radio" id="3star3" name="livingroom" value="3" /><label for="3star3" title="Meh"></label>
+                            <input type="radio" id="3star2" name="livingroom" value="2" /><label for="3star2" title="Kinda bad"></label>
+                            <input type="radio" id="3star1" name="livingroom" value="1" /><label for="3star1" title="Sucks big time"></label>
+                        </fieldset>
+                    </div>
+                    <div class="duties"></div>
+                    <div >
+                        <fieldset class="rating1">
+                            <legend>Kitchen</legend>
+                            <input type="radio" id="4star5" name="kitchen" value="5" /><label for="4star5" title="Rocks!"></label>
+                            <input type="radio" id="4star4" name="kitchen" value="4" /><label for="4star4" title="Pretty good"></label>
+                            <input type="radio" id="4star3" name="kitchen" value="3" /><label for="4star3" title="Meh"></label>
+                            <input type="radio" id="4star2" name="kitchen" value="2" /><label for="4star2" title="Kinda bad"></label>
+                            <input type="radio" id="4star1" name="kitchen" value="1" /><label for="4star1" title="Sucks big time"></label>
+                        </fieldset>
+                    </div>
+
+                          <div> 
+                          <fieldset class="duties">
                               <label> <h4>Duties Omitted</h4> </label>
                               <input type="checkbox" name="flunks[]" value=" bedroom floor dirty">Bedroom floor not cleaned <br />
                               <input type="checkbox" name="flunks[]" value=" bathroom floor dirty">Bathroom floor not cleaned<br />
@@ -81,7 +82,7 @@
                               <input type="checkbox" name="flunks[]" value="door unlocked"> Door not locked <br />
                               <input type="checkbox" name="flunks[]" value="alarm not set"> Alarm not set <br />           
                   
-                          </fieldset>
+                          </fieldset></div>
                       {{-- <div><input type="submit">Send Review</div>   --}}
                       <button type="submit">Send Review</button>     
                     </form>
